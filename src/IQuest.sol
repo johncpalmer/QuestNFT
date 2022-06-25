b8acca0d1416e2b64b95a10017e61c5e5b57d3b6
John Palmer <johnpalmer@Johns-MacBook-Pro.local>
2022-06-24 21:34:30 -0400

    make beatLevel write function

--- src/IQuest.sol
+++ src/IQuest.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-// Interface for a new quest, which can be beaten to increase xp.
-interface IQueset {
+import "./QuestNFT.sol";
 
-    // Implements logic to tell if this tokenId has beaten this level.
-    function isCompleted(
+// Interface for a new quest, which can be beaten to increase xp.
+interface IQuest {
+
+    // Implements logic to tell if this tokenId has beaten this level. Has ability to call changeScore() in parent.
+    function beatQuest(
         uint256 tokenId,
         // Optional arbitrary data the user can pass in to verify a level was completed.
         bytes calldata userData
-    ) external returns (bool);
+    ) external payable returns (bool);
 }
